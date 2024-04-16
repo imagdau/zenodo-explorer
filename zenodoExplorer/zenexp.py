a5846c2cc0ddef29e47e4ee17f0c8c8eb551af0f
Ioan-Bogdan Magdau <i.b.magdau@gmail.com>
2024-04-16 14:48:36 +0100

    refactored everything and used zenodo info checksum

--- zenodoExplorer/zenexp.py
+++ zenodoExplorer/zenexp.py
@@ -12,60 +12,60 @@ class ze:
     def __init__(self, ACCESS_TOKEN, recIDs):
         self.ACCESS_TOKEN = ACCESS_TOKEN
         self.recIDs = recIDs
-        self.urls = dict()
+        self.info = dict()
         self.zdb = zdb()
         for recID in self.recIDs:
-            self.urls[recID] = dict()
+            self.info[recID] = dict()
             r = requests.get('https://zenodo.org/api/deposit/depositions/'+str(recID), params={'access_token': self.ACCESS_TOKEN})
-            self.urls[recID].update({db['filename']:db['links']['download'] for db in r.json()['files']})
-            self.urls[recID] = dict(sorted(self.urls[recID].items()))
+            self.info[recID].update({db['filename']:(db['checksum'], db['links']['download']) for db in r.json()['files']})
+            self.info[recID] = dict(sorted(self.info[recID].items()))
     
-    def get_chunk(self, recID, fname, compare_hash):
-        url = self.urls[recID][fname]
+    def get_chunk(self, recID, fname):
+        new_checksum, url = self.info[recID][fname]
         fbase, fext = os.path.splitext(fname)
         os.makedirs('.cache/'+str(recID), exist_ok=True)
         temp_dest = '.cache/'+str(recID)+'/'+fname
         final_dest = '.cache/'+str(recID)+'/'+fbase        
         if fext == '.yml':
             final_dest = temp_dest
-        if not os.path.exists(final_dest) or compare_hash:
-            rewrite = True
+        download = False
+        if os.path.exists(final_dest):
+            with open(final_dest+'.hash', 'r') as f:
+                old_checksum = f.readline().strip()
+            if new_checksum != old_checksum:
+                download = True
+        else:
+            download = True
+        if download:
             file_response = requests.get(url, params={'access_token': self.ACCESS_TOKEN})
-            new_hash = md5(file_response.content).hexdigest()
-            if os.path.exists(final_dest+'.hash'):
-                with open(final_dest+'.hash', 'r') as f:
-                    old_hash = f.readline().strip()
-                    if new_hash == old_hash:
-                        rewrite = False
-            if rewrite:
-                with open(final_dest+'.hash', 'w') as f:
-                    f.write(new_hash+'\n')
-                with open(temp_dest, 'wb') as f:
-                    f.write(file_response.content)
-                if fext == '.zip':
-                    with zipfile.ZipFile(temp_dest, 'r') as zip_ref:
-                        zip_ref.extractall(final_dest)
-                        os.remove(temp_dest)
+            with open(final_dest+'.hash', 'w') as f:
+                f.write(new_checksum+'\n')
+            with open(temp_dest, 'wb') as f:
+                f.write(file_response.content)
+            if fext == '.zip':
+                with zipfile.ZipFile(temp_dest, 'r') as zip_ref:
+                    zip_ref.extractall(final_dest)
+                    os.remove(temp_dest)
         return final_dest
 
-    def cache_all_data(self, compare_hash=False):
-        for recID in self.urls:
+    def cache_all_data(self):
+        for recID in self.info:
             print(recID)
-            for url_key in tqdm(self.urls[recID]):
-                self.get_chunk(recID, url_key, compare_hash)
+            for fname in tqdm(self.info[recID]):
+                self.get_chunk(recID, fname)
                 
-    def read_zdb(self, compare_hash=True):
-        for recID in self.urls:
-            final_dest = self.get_chunk(recID, 'data.yml', compare_hash)
+    def read_zdb(self):
+        for recID in self.info:
+            final_dest = self.get_chunk(recID, 'data.yml')
             with open(final_dest, 'r') as file:
                 zdb_dict = yaml.safe_load(file)
                 self.zdb.update(zdb_dict, recID)
 
-    def read_dat_files(self, tag, compare_hash=False):
+    def read_dat_files(self, tag):
         dat = self.zdb.get_dat(tag)
         recID = int(dat.get_recID())
         fname = dat.zip+'.zip'
-        final_dest = self.get_chunk(recID, fname, compare_hash)
+        final_dest = self.get_chunk(recID, fname)
         pattern = dat.file.split(',')
         flist = []
         for p in pattern:
